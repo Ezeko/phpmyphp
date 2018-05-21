3d8c0a19dc685a080910cf43e7bd03df27268552
Adejobi Ezekiel <31517324+Ezeko@users.noreply.github.com>
2018-05-21 21:17:30 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My php code</title>
+	<li><ul>Home |</ul><ul>About |</ul><ul><a href="login.php">Login</a></ul></li>
+</head>
+<body>
+	<?php
+	include ("header.php");
+	?>
+	<p> lorem ipsum	</p>
+	<p> Lorem ipsum</p>
+</body>
+</html>
